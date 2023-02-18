8e7dee918029bf2feddd3f1dc82fd1519ac0af09
RYertayev <yertayevrk@gmail.com>
2023-02-19 05:15:26 +0600

    fps

---
+++ Final/Assets/Scripts/FPS_Player.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPS_Player : MonoBehaviour
+{
+    public float speed;
+    bool isGrounded;
+    public float jumpForce;
+    public GameObject aimActive;
+    Rigidbody rb;
+    public DialogueManager dialoguemanager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();   
+        isGrounded = false;    
+        aimActive.SetActive(false); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            aimActive.SetActive(true);
+        }
+        if(Input.GetMouseButtonDown(0))
+        {
+            //anim
+            //playsound
+            print("worked");
+        }
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded == false)
+        {
+            rb.AddForce(new Vector3(0, 3f, 0));
+            isGrounded = true;
+        }
+    }
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 move = new Vector3(horizontal, 0f, vertical);
+        transform.Translate(move * speed);
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        isGrounded = false;
+
+        if(collision.gameObject.tag == "soldier")
+        {
+            dialoguemanager.StartDialogue();
+            dialoguemanager.isActive = true;
+        }
+    }
+    
+}
--- Final/Assets/Scripts/Player.cs
+++ Final/Assets/Scripts/Player.cs
@@ -6,6 +6,7 @@ public class Player : MonoBehaviour
 {
     public float speed;
     public float JumpForce;
+    bool isGrounded;
     public GameObject targetActive;
     public Rigidbody rb;
     public DialogueManager dialoguemanager;
@@ -13,27 +14,30 @@ public class Player : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        isGrounded = false;
     }
 
     // Update is called once per frame
     void Update()
+    {
+       //Active target img
+       if(Input.GetKeyDown(KeyCode.F))
+       {
+          targetActive.SetActive(true);
+       }
+       if(Input.GetKeyDown(KeyCode.Space) && isGrounded == false){
+          rb.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
+          isGrounded = true;
+       }
+       
+    }
+    void FixedUpdate()
     {
         //basic movement
        float x = Input.GetAxis("Horizontal");
        float z = Input.GetAxis("Vertical");
        Vector3 move = new Vector3(x, 0, z);
        transform.Translate(move * speed);
-
-       //Active target img
-       if(Input.GetKeyDown(KeyCode.F))
-       {
-          targetActive.SetActive(true);
-       }
-
-       if(Input.GetKeyDown(KeyCode.Space))
-       {
-          rb.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
-       }
     }
     void OnCollisionEnter(Collision collision)
     {
@@ -42,5 +46,6 @@ public class Player : MonoBehaviour
             dialoguemanager.StartDialogue();
             dialoguemanager.isActive = true;
         }
+        isGrounded = false;
     }
 }
