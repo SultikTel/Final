911b79c5d53a8c1d0870e7d843653bd8884ad4bd
RYertayev <yertayevrk@gmail.com>
2023-02-22 02:06:50 +0600

    reload weapon

---
+++ Final/Assets/Scripts/Weapon.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public GameObject weapon1;
+    public GameObject weapon2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        weapon2.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            weapon1.SetActive(false);
+            weapon2.SetActive(true);
+        }   
+    }
+}
