f976e87dabf0ffe678f806d3b16df0b1578b5e87
Kulbayev Abylaikhan <abylay2001@gmail.com>
2023-02-28 00:37:31 +0600

    MainMenu

---
+++ Final/Assets/Scripts/LoadPrefs.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class LoadPrefs : MonoBehaviour
+{
+    [Header("General Setting")]
+    [SerializeField] private bool canUse = false;
+    [SerializeField] private MenuController menuController;
+
+    [Header("Volume Setting")]
+    [SerializeField] private TMP_Text volumeTextValue = null;
+    [SerializeField] private Slider volumeSlider = null;
+
+    [Header("Brightness Setting")]
+    [SerializeField] private Slider brightnessSlider = null;
+    [SerializeField] private TMP_Text brightnessTextValue = null;
+
+    [Header("Quality level Setting")]
+    [SerializeField] private TMP_Dropdown qualityDropdown;
+
+    [Header("Fullscreen Setting")]
+    [SerializeField] private Toggle fullscreenToggle;
+
+    [Header("Sensitivity Setting")]
+    [SerializeField] private TMP_Text controllerSenTextValue = null;
+    [SerializeField] private Slider controllerSenSlider = null;
+
+
+    [Header("Invert Y Setting")]
+    [SerializeField] private Toggle invertYToggle = null;
+
+    private void Awale()
+    {
+        if(canUse == true)
+        {
+            if(PlayerPrefs.HasKey("masterVolume"))
+            {
+                float localVolume = PlayerPrefs.GetFloat("masterVolume");
+
+                volumeTextValue.text = localVolume.ToString("0.0");
+                volumeSlider.value = localVolume;
+                AudioListener.volume = localVolume;
+            }else
+            {
+                menuController.ResetButton("Audio");
+            }
+
+            if(PlayerPrefs.HasKey("masterQuality"))
+            {
+                int localQuality = PlayerPrefs.GetInt("masterQuality");
+                qualityDropdown.value = localQuality;
+                QualitySettings.SetQualityLevel(localQuality);
+            }
+            if(PlayerPrefs.HasKey("masterFullscreen"))
+            {
+                int localFullscreen = PlayerPrefs.GetInt("masterFullscreen");
+
+                if(localFullscreen == 1)
+                {
+                    Screen.fullScreen = true;
+                    fullscreenToggle.isOn = true;
+                }
+                else
+                {
+                    Screen.fullScreen = false;
+                    fullscreenToggle.isOn = false;
+                }
+            }
+            if(PlayerPrefs.HasKey("masterBrightness"))
+            {
+                float localBrightness = PlayerPrefs.GetFloat("masterBrightness");
+
+                brightnessTextValue.text = localBrightness.ToString("0.0");
+                brightnessSlider.value = localBrightness;
+            }
+            if(PlayerPrefs.HasKey("masterInvertY"))
+            {
+                if(PlayerPrefs.GetInt("masterInvertY") == 1)
+                {
+                    invertYToggle.isOn = true;
+                }else
+                {
+                    invertYToggle.isOn = false;
+                }
+            }
+        }
+    }
+
+}
