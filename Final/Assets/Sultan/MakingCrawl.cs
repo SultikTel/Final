8f485580ecbba6007f72c9008008dfe97f75eb42
SultikTel <sultiktel@gmail.com>
2023-03-01 00:35:24 +0600

    AnimChernovik

---
+++ Final/Assets/Sultan/MakingCrawl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakingCrawl : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public int operation;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<NpsTrain>() != null)
+        {
+            if (operation == 0)
+            {
+                other.gameObject.GetComponent<NpsTrain>().Idle();
+            }
+            if (operation == 1) {
+                other.gameObject.GetComponent<NpsTrain>().Walk(); 
+            }
+            if (operation == 2)
+            {
+                other.gameObject.GetComponent<NpsTrain>().Crawl();
+            }
+            if (operation == 3)
+            {
+                other.gameObject.GetComponent<NpsTrain>().Jump();
+            }
+        }
+
+        
+
+
+    }
+
+    
+   
+}
---
+++ Final/Assets/Sultan/NpsTrain.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpsTrain : MonoBehaviour
+{
+    Animator animator;
+    public Move player;
+    
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        player = transform.parent.GetComponent<Move>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Walk();
+        }
+    }
+
+    
+    public void Crawl()
+    {
+        player.move = true;
+        animator.SetInteger("AnimIndex", 3);
+        
+    }
+
+    public void Jump()
+    {
+        player.move = true;
+        animator.SetInteger("AnimIndex", 2);
+
+    }
+
+    public void Walk()
+    {
+        player.move = true;
+        animator.SetInteger("AnimIndex", 1);
+
+    }
+
+    public void Idle()
+    {
+        player.move = false;
+        animator.SetInteger("AnimIndex", 0);    
+
+    }
+    
+
+
+}
