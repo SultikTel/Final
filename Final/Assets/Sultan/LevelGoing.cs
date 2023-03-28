59be92b3b4cc35aa758848851be534c53253c588
SultikTel <sultiktel@gmail.com>
2023-03-29 01:05:06 +0600

    FirstStepsOfGameWay

---
+++ Final/Assets/Sultan/LevelGoing.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGoing : MonoBehaviour
+{
+    public EnemyBySultan[] enemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemy = Object.FindObjectsOfType<EnemyBySultan>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
