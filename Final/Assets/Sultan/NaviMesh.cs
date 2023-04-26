2f4ae28fafc26a6f7fae60ec5331fde440db942e
SultikTel <sultiktel@gmail.com>
2023-04-27 01:24:30 +0600

    testNavMesh

---
+++ Final/Assets/Sultan/NaviMesh.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NaviMesh : MonoBehaviour
+{
+    public Transform place_one;
+    public Transform place_two;
+    public bool goPlaceOne;
+    public bool canGo;
+    public NavMeshAgent navmesh;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        ChangePosition();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        navmesh.destination = place_one.position;
+        
+    }
+
+    IEnumerator ChangePosition()
+    {
+        canGo = true;
+        
+        yield return new WaitForSeconds(8f);
+        canGo = false;
+
+        yield return new WaitForSeconds(5f);
+        if (goPlaceOne)
+        {
+            goPlaceOne = false;
+        }
+        else
+        {
+            goPlaceOne = true;
+        }
+
+        ChangePosition();
+    }
+}
