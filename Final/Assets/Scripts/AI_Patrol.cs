2dd523bfc5e7495cd7420b00274d62b7d3df116c
RYertayev <yertayevrk@gmail.com>
2023-05-19 17:13:39 +0600

    patrool

--- Final/Assets/Scripts/AI_Patrol.cs
+++ Final/Assets/Scripts/AI_Patrol.cs
@@ -1,30 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
 
 public class AI_Patrol : MonoBehaviour
 {
-    public NavMeshAgent agent;
-    int i;
-    public List<Transform> target;
+    public float speed;
+    public Transform[] points;
+    int random_point;
     // Start is called before the first frame update
     void Start()
     {
-        agent = GetComponent<NavMeshAgent>();
+        random_point = Random.Range(0, points.Length); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(agent.transform.position == agent.pathEndPosition)
+        transform.position = Vector3.MoveTowards(transform.position, points[random_point].position, speed * Time.deltaTime);
+        if(transform.position == points[random_point].position)
         {
-            TargetsUpdate();
+            random_point = Random.Range(0, points.Length);
         }
-        agent.SetDestination(target[i].position);
     }
-    void TargetsUpdate()
-    {
-        i = Random.Range(0, target.Count);
-    }
+
 }
