385b41e9458cc4dc3a810973a6c2ceb9499d99f6
RYertayev <yertayevrk@gmail.com>
2023-06-08 03:40:07 +0600

    Added 6 panfolifses

---
+++ Final/Assets/Scripts/for soviet_soldier/Soviet_Soldier_3.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Soviet_Soldier_3 : MonoBehaviour
+{
+    public Animator animator;
+    public bool anim_bool = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        anim_bool = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(anim_bool == true)
+        {
+            animator.SetBool("idle_crouching", true);
+        }else {
+            animator.SetBool("idle_crouching", false);
+        }
+    }
+}
---
+++ Final/Assets/Scripts/for soviet_soldier/Soviet_Soldier_4.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Soviet_Soldier_4 : MonoBehaviour
+{
+    public Animator animator;
+    public bool anim_bool = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        anim_bool = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(anim_bool == true)
+        {
+            animator.SetBool("rifle_idle_two", true);
+        }else {
+            animator.SetBool("rifle_idle_two", false);
+        }
+    }
+}
