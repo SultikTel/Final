ca45fd1250eb69d4febea524fdadb0549f936e04
RYertayev <yertayevrk@gmail.com>
2023-06-12 16:24:20 +0600

    sniper

---
+++ Final/Assets/Scripts/scripts for second level/Sniper.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Sniper : MonoBehaviour
+{
+   public float speed;
+    private float jumpHeight = 1.0f;
+    //private float jumpTime = 0.5f;
+    private float groundDistance = 0.2f;
+    public LayerMask groundMask;
+    public bool isJumping = false;
+    private float jumpVelocity = 0.0f;
+    private float gravity = -9.8f;
+    public CharacterController controller;
+    private Vector3 velocity;
+    public Transform CheckGround;
+    public Camera camera;
+
+    //Health script
+    public float maxHealth = 100f;
+    public float currentHealth = 0f;
+    public float damage = 1f;
+    public HealthBar hb;
+    public Image blood;
+    Color alphaColor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        alphaColor = blood.color;
+        hb.SetHealth(currentHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal  = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 move = transform.right * horizontal + transform.forward * vertical;
+        controller.Move(move * speed * Time.deltaTime);
+
+        bool isGrounded = Physics.CheckSphere(CheckGround.position, groundDistance, groundMask);
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            isJumping = true;
+            jumpVelocity = Mathf.Sqrt(-2.0f * gravity * jumpHeight);
+        }
+        if(isGrounded && velocity.y > 10f)
+        {
+            isJumping = false;
+            velocity.y = -2f;
+        }
+        if(isJumping)
+        {
+            controller.Move(Vector3.up * (jumpVelocity * Time.deltaTime));
+            jumpVelocity += gravity * Time.deltaTime;
+        }
+         //SitDown
+        if(Input.GetKey(KeyCode.C))
+        {
+            controller.height = 1f;
+            camera.transform.localPosition = new Vector3(0, -0.36f, 0);
+        }else {
+            controller.height = 2f;
+             camera.transform.localPosition = new Vector3(0, 0.2367195f, 0);
+        }
+    }
+    public void TakeDamage()
+    {
+        currentHealth -= damage;
+        hb.SetHealth(currentHealth);
+        StartCoroutine(BloodEffect());
+        if(currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+    void Die()
+    {
+        print("Player is dead");
+    }
+    IEnumerator BloodEffect()
+    {
+        alphaColor.a += .1f;
+        blood.color = alphaColor;
+        yield return new WaitForSeconds(5f);
+        alphaColor.a -= .1f;
+        blood.color = alphaColor;
+    }
+}
+
---
+++ Final/Assets/Scripts/scripts for second level/sniper_fire.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class sniper_fire : MonoBehaviour
+{
+    public Camera MainCamera;
+    public GameObject bullet_impact;
+    public AudioSource gameManager;
+    public AudioClip shoot;
+    public AudioClip sound2;
+    public AudioSource walking_sound;
+    public GameObject message1;
+    public int current_ammo;
+    public int max_ammo = 5;
+    public int damage;
+    private bool isReloading = false;
+    public GameObject camera_first;
+    public GameObject camera_aim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        message1.SetActive(false);
+        current_ammo = max_ammo;
+        //camera_aim.SetActive(false);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //shoot
+        if(Input.GetMouseButtonDown(0) && current_ammo > 0)
+        {
+            current_ammo -= 1;
+            Shoot(); //Active shoot function
+            gameManager.PlayOneShot(shoot); // sound while shooting
+        }
+        //Reload
+        if(isReloading) 
+           return;
+        if(current_ammo == 0) 
+        {
+            PlaySoundReload();
+            StartCoroutine(Reloading());
+            return;
+        }
+        
+       //Aim 
+       if(Input.GetMouseButtonDown(1))
+       {
+            AimOn();
+       }
+       if(Input.GetMouseButtonUp(1))
+       {
+           Aimoff();
+       }
+       
+     
+    }
+    void AimOn()
+    {
+       camera_aim.SetActive(true); 
+       camera_first.SetActive(false);
+    }
+    void Aimoff()
+    {
+       camera_first.SetActive(true);
+       camera_aim.SetActive(false); 
+    }
+    void Shoot()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(MainCamera.transform.position, MainCamera.transform.forward, out hit, 100))
+        {
+            Debug.Log(hit.transform.name); //to print of name gameobject to triggered by weapon
+
+            //Edited by Sultan
+
+            EnemyBySultan enemyBySultan = hit.collider.GetComponent<EnemyBySultan>();
+            if (enemyBySultan != null)
+            {
+                enemyBySultan.die();
+            }
+            //the ended by sultan
+
+            Target target = hit.transform.GetComponent<Target>();
+            if(target != null)
+            {
+                target.TakeDamage(damage);
+            }
+            enemy_death enemy = hit.transform.GetComponent<enemy_death>();
+            if(enemy != null)
+            {
+                enemy.TakeDamage(damage);
+            }
+            HealthEnemy health_enemy = hit.transform.GetComponent<HealthEnemy>();
+            if(health_enemy != null)
+            {
+                health_enemy.TakeDamage(damage);
+            }
+            
+            
+            if(hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * 50);
+            }
+            GameObject impact_clone = Instantiate(bullet_impact, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impact_clone, 1.5f);   
+        }
+    }
+    void PlaySoundReload()
+    {
+        gameManager.PlayOneShot(sound2); 
+    }
+    IEnumerator Reloading()
+    {
+        isReloading = true;
+        message1.SetActive(true);
+        yield return new WaitForSeconds(3f);
+        current_ammo = max_ammo;
+        message1.SetActive(false);
+        isReloading = false;
+    }
+}
+
+
