414b35bf1b46bd60090a76a32ccf7a3793d599af
RYertayev <yertayevrk@gmail.com>
2023-06-13 14:37:34 +0600

    first_cut_scene

---
+++ Final/Assets/Scripts/for cutscene/Intro.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Intro : MonoBehaviour
+{
+    public int duringTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(OpenScene());
+    }
+    void Update()
+    {
+        Skip(); 
+    }
+    IEnumerator OpenScene()
+    {
+        yield return new WaitForSeconds(duringTime);
+        SceneManager.LoadScene(2);
+    }
+    void Skip()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene(2);
+        }
+    }
+    
+}
