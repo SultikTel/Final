fc6e33a859208e2887ad833b1f6fc4a167fbd0b7
RYertayev <yertayevrk@gmail.com>
2023-06-19 02:49:08 +0600

    mobile first level

---
+++ Final/Assets/Scripts mobile/Bazooka_mobile.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Bazooka_mobile : MonoBehaviour
+{
+    private bool isFire;
+    public Camera MainCamera;
+    public int current_ammo;
+    public int magazine;
+    public int count;
+    public GameManager2 gamemanager;
+    public AudioClip shoot;
+    public GameObject reload_text;
+    public ParticleSystem fire;
+    public ParticleSystem Vzriv;
+    public GameObject vzriv_gameobject;
+    public GameObject bullet_impact;
+    private GameObject panzer;
+    public GameObject panzer_destroyed;
+    private bool exploded;
+
+    //WaveController to count enemy otryad
+    public GameObject wavecontroller;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fire.Stop();
+        Vzriv.Stop();
+        isFire = false;
+        current_ammo = magazine;
+        exploded = false;
+        reload_text.GetComponent<Text>().text = "Bazooka Ammo: " + current_ammo.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isFire && current_ammo > 0)
+        {
+            Shoot(); //Call method shoot     
+        if (Physics.Raycast(MainCamera.transform.position, MainCamera.transform.forward, out RaycastHit Hitinfo, 100))
+        {
+            //Debug.Log(Hitinfo.transform.name);
+            fire.Play(); //play particles
+            gamemanager.PlaySound(shoot); //play sound shoot
+            current_ammo -= 1;
+            reload_text.GetComponent<Text>().text = "Bazooka Ammo: " + current_ammo.ToString();
+            if (Hitinfo.transform.CompareTag("Panzer")) //to destroy tanks
+            {
+                count += 1;
+                panzer = Hitinfo.transform.gameObject;
+            }
+            if(Hitinfo.transform.CompareTag("germans")) //to destroy germans soldier
+            {
+                print("germans");
+                Target health_bar_enemy = Hitinfo.transform.GetComponent<Target>();
+                health_bar_enemy.Die();
+            }
+            if (Hitinfo.rigidbody != null) // to make objects move from bullets
+            {
+                Hitinfo.rigidbody.AddForce(-Hitinfo.normal * 80);
+            }
+
+            GameObject impact_clone = Instantiate(bullet_impact, Hitinfo.point, Quaternion.LookRotation(Hitinfo.normal)); //to put effects billet impact
+            Destroy(impact_clone, 1.5f);
+
+            GameObject boom = Instantiate(vzriv_gameobject, Hitinfo.point, Quaternion.LookRotation(Hitinfo.normal)); //to put effect vzriv
+            Destroy(boom, 2f);
+            isFire = false;
+        } 
+        }
+        if(current_ammo == 0)
+        {
+            isFire = false;
+        }
+        if (count == 3 && !exploded && panzer != null) //Check if our count bullets = 3 and he triggered by tank then play effects vzriv
+        {
+            wavecontroller.GetComponent<WaveController>().EnemyDead();
+            exploded = false;
+            count = 0;
+            StartCoroutine(Panzer_death());
+        }
+    }
+
+    IEnumerator Panzer_death()
+    {
+        Instantiate(Vzriv, panzer.transform.position, Quaternion.identity);
+        yield return new WaitForSeconds(.7f);
+        panzer.SetActive(false);
+        panzer_destroyed.SetActive(true);
+        Instantiate(panzer_destroyed, panzer.transform.position, panzer.transform.rotation);
+    }
+    public void Shoot()
+    {
+        isFire = true;
+    }
+    public void ActiveReloadText()
+    {
+        reload_text.SetActive(true);
+    }
+    public void Deactivation()
+    {
+        reload_text.SetActive(false);
+    }
+}
---
+++ Final/Assets/Scripts mobile/FPS_mobile.cs
@@ -0,0 +1,153 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class FPS_mobile : MonoBehaviour
+{
+    public int speed;
+    public FixedJoystick joystick;
+    public CharacterController charactercontroller;
+    public Camera main_camera;
+    private bool isJumping;
+    private float gravity = 10f;
+    private float jumpTimer = 0f;
+    private float jumpTime = 1f;
+    private float jumpHeight = 6f;
+    private float verticalspeed;
+    public GameObject bazooka;
+    public AudioSource source;
+    public AudioClip sound_reload;
+    public AudioClip first_aid_kit;
+     //Health script
+    public float maxHealth = 100f;
+    public float currentHealth = 0f;
+    public float damage = 1f;
+    public HealthBar hb;
+    public Image blood;
+    Color alphaColor;
+    public GameObject door_model;
+    public GameObject hint_text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isJumping = false;
+        charactercontroller = GetComponent<CharacterController>();
+        currentHealth = maxHealth;
+        alphaColor = blood.color;
+        hb.SetHealth(currentHealth);
+        hint_text.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 move = transform.right * joystick.Horizontal + transform.forward * joystick.Vertical;
+        charactercontroller.Move(move * speed * Time.deltaTime);
+    
+        if(isJumping)
+        {
+            charactercontroller.Move(new Vector3(0f, verticalspeed, 0f) * Time.deltaTime);
+            verticalspeed -= gravity * Time.deltaTime;
+            if(charactercontroller.isGrounded)
+            {
+                isJumping = false;
+            }
+        }else if(!charactercontroller.isGrounded)
+        {
+            charactercontroller.Move(new Vector3(0f, verticalspeed, 0f) * Time.deltaTime);
+            verticalspeed -= gravity * Time.deltaTime;
+        }else {
+            isJumping = false;
+        }
+    }
+
+    public void Jump()
+    {
+        if(isJumping || !charactercontroller.isGrounded)
+        {
+            return;
+        }
+        isJumping = true;
+        verticalspeed = jumpHeight;
+    }
+    public void CroachDown()
+    {
+        charactercontroller.height = 1f;
+        main_camera.transform.localPosition = new Vector3(0, -0.36f, 0);
+    }
+    public void CroachUp()
+    {
+        charactercontroller.height = 2f;
+        main_camera.transform.localPosition = new Vector3(0, 0.2367195f, 0);
+    }
+
+    public void TakeDamage()
+    {
+        currentHealth -= damage;
+        hb.SetHealth(currentHealth);
+        StartCoroutine(BloodEffect());
+        if(currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+    public void TakeDamageFromTank(float damage)
+    {
+        currentHealth -= damage;
+        hb.SetHealth(currentHealth);
+        StartCoroutine(BloodEffect());
+        if(currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+    void Die()
+    {
+        print("Player is dead");
+    }
+    IEnumerator BloodEffect()
+    {
+        alphaColor.a += .1f;
+        blood.color = alphaColor;
+        yield return new WaitForSeconds(5f);
+        alphaColor.a -= .1f;
+        blood.color = alphaColor;
+    }
+
+    public void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if(hit.collider.CompareTag("Bullets"))
+        {
+            if(bazooka.GetComponent<Bazooka_mobile>().current_ammo == 0)
+            {
+                source.PlayOneShot(sound_reload);
+                bazooka.GetComponent<Bazooka_mobile>().current_ammo = 3;
+                bazooka.GetComponent<Bazooka_mobile>().reload_text.GetComponent<Text>().text = "Bazooka Ammo: " + bazooka.GetComponent<Bazooka_mobile>().current_ammo.ToString();
+            }
+        }
+        
+        if(hit.collider.CompareTag("door"))
+        {
+                print("rabotaet");
+                //GameObject door_model = GameObject.Find("OpenDoor");
+                door_model.GetComponent<Animator>().SetBool("Open_door", true);
+        }
+        if(hit.collider.CompareTag("MedKit"))
+        {
+            StartCoroutine(show_hint());
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                hint_text.SetActive(false);
+                source.PlayOneShot(first_aid_kit);
+                currentHealth = maxHealth;
+                hb.SetHealth(currentHealth);
+            }
+        }
+    }
+    IEnumerator show_hint()
+    {
+        hint_text.SetActive(true);
+        yield return new WaitForSeconds(2.5f);
+        hint_text.SetActive(false);
+    }
+}
