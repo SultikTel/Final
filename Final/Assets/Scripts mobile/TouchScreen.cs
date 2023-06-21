1538945301f973ed2ef2af015711efd0ed7a28e4
RYertayev <69892190+RYertayev@users.noreply.github.com>
2023-06-21 22:59:35 +0600

    mobile mode

---
+++ Final/Assets/Scripts mobile/TouchScreen.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchScreen : MonoBehaviour
+{
+    public float rotationSpeed;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if(touch.phase == TouchPhase.Moved)
+            {
+                float rotatValue = touch.deltaPosition.x * rotationSpeed;
+
+                transform.Rotate(0f, rotatValue, 0f);
+            }
+        }
+    }
+}
