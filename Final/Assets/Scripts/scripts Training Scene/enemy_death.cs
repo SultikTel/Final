a3a7204a6fbdfeba298c55c49df4e1ae74fc19b5
RYertayev <69892190+RYertayev@users.noreply.github.com>
2023-06-26 04:56:35 +0600

    enemy_death

--- Final/Assets/Scripts/scripts Training Scene/enemy_death.cs
+++ Final/Assets/Scripts/scripts Training Scene/enemy_death.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using UnityEngine.SceneManagement;
 public class enemy_death : MonoBehaviour
 {
     public int health = 50;
@@ -27,9 +27,10 @@ public class enemy_death : MonoBehaviour
     void Die()
     {
         win_text.SetActive(true);
-        button.SetActive(true);
+        //button.SetActive(true);
         gameObject.SetActive(false);
         NPC_Ragdoll.SetActive(true);
         Instantiate(NPC_Ragdoll, transform.position, transform.rotation);
+        SceneManager.LoadScene("TrainingLevel");
     }
 }
